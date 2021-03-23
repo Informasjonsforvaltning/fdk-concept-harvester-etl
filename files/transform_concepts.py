3f29baaee1979270769454f9051867c7185231e7
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-23 14:24:06 +0100

    chore: etl for removing nav concepts part 1

--- files/transform_concepts.py
+++ files/transform_concepts.py
@@ -8,49 +8,25 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
 args = parser.parse_args()
-update_dates = os.environ["TO_BE_UPDATED"] == 'dates'
+
+inputfile = 'nav_concepts.json'
+conceptsfileName = args.outputdirectory + "concepts.json"
+outputfileName = args.outputdirectory + "unmatched_concepts.json"
 
 
-def transform(inputfile, inputfile_mongo):
-
-    concepts = openfile(inputfile)
-    mongo_ids = openfile(inputfile_mongo)
-
-    array = concepts["hits"]["hits"]
-
-    print("Number of concepts:" + str(len(array)))
-    transformed = {}
-    failed = {}
-    for concept in array:
-        uri = concept["_source"].get("identifier")
-        if mongo_ids.get(uri):
-            transformed[uri] = fields_to_change(concept)
-        else:
-            failed[uri] = "Not found in mongo"
-
-    failed_transform = args.outputdirectory + "failed_transform.json"
-    with open(failed_transform, 'w', encoding="utf-8") as failed_file:
-        json.dump(failed, failed_file, ensure_ascii=False, indent=4)
-    return transformed
-
-
-def openfile(file_name):
-    with open(file_name) as json_file:
-        return json.load(json_file)
-
-
-def fields_to_change(elastic_concept):
-    if update_dates is True:
-        return {"issued": elastic_concept["_source"]["harvest"]["firstHarvested"],
-                "modified": elastic_concept["_source"]["harvest"]["lastHarvested"]}
-    else:
-        return {"fdkId": elastic_concept["_id"]}
-
-
-inputfileName = args.outputdirectory + "concepts.json"
-inputfileNameMongo = args.outputdirectory + "mongo_concepts_id.json"
-outputfileName = args.outputdirectory + "concepts_transformed.json"
-
-
-with open(outputfileName, 'w', encoding="utf-8") as outfile:
-    json.dump(transform(inputfileName, inputfileNameMongo), outfile, ensure_ascii=False, indent=4)
+exclude = {}
+count = 0
+with open(conceptsfileName) as concepts_file:
+    data = json.load(concepts_file)
+    array = data["hits"]["hits"]
+    with open(inputfile) as nav_concepts_file:
+        concepts = json.load(nav_concepts_file)
+        for elastic_concept in array:
+            if elastic_concept["_source"]["identifier"][:27] == 'https://data.nav.no/begrep/':
+                nav_concept = elastic_concept["_source"]["identifier"]
+                if nav_concept not in concepts:
+                    count += 1
+                    exclude[count] = nav_concept
+                    print(str(exclude))
+        with open(outputfileName, 'w', encoding="utf-8") as outfile:
+            json.dump(exclude, outfile, ensure_ascii=False, indent=4)
